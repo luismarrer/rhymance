e58335326ede4ddca73ab223b73431658a06f414
Luis Marrero <luismarrer.dev@gmail.com>
2026-09-13 18:50:53 -0400

    ci: build workspace packages before linting and ignore workspace prefix in import/no-unresolved

--- apps/app/eslint.config.js
+++ apps/app/eslint.config.js
@@ -5,6 +5,11 @@ const expoConfig = require("eslint-config-expo/flat");
 module.exports = defineConfig([
   expoConfig,
   {
+    rules: {
+      'import/no-unresolved': ['error', { ignore: ['^@rhymance/'] }],
+    },
+  },
+  {
     ignores: ["dist/*"],
   }
 ]);
